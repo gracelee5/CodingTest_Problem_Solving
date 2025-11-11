319c69e095aba2557a3c78c0ec4dfbb4cd9c6f8d
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2025-11-11 17:28:39 +0900

    [level 2] Title: 타겟 넘버, Time: 36.40 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,21 @@
+function solution(numbers, target) {
+    var answer = 0;
+    const length = numbers.length;
+    
+    
+    function dfs(count, sum){
+        if (count == length){
+            if(target == sum){
+                answer ++;
+            }
+            return;
+        }
+    dfs(count + 1, sum + numbers[count]);
+    dfs(count + 1, sum - numbers[count]);
+    }
+    
+    dfs(0,0);
+    
+    
+    return answer;
+}
