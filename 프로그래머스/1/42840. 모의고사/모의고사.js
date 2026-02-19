6cddf45b7c87148873a30f9b5f9d638642608e5f
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2026-02-19 14:27:12 +0900

    [level 1] Title: 모의고사, Time: 2.89 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -6,18 +6,19 @@ function solution(answers) {
         [3,3,1,1,2,2,4,4,5,5]
     ];
     
-    let score = [0,0,0];
+    var score = [0,0,0];
     
-    for (let i = 0; i < answers.length; i++){
+    for (let i = 0; i<answers.length; i++){
         for (let j = 0; j <patterns.length; j++){
             if(answers[i] == patterns[j][i%patterns[j].length]){
                 score[j]++;
             }
-        }        
+        }
     }
-    let max = Math.max(...score);
-    for ( let i = 0; i <patterns.length; i++){
-        if (max === score[i]){
+    
+    const max = Math.max(...score);
+    for(let i = 0; i<score.length; i++){
+        if(max == score[i]){
             answer.push(i+1);
         }
     }
