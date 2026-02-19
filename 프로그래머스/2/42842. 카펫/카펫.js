8d743c03cb32161e07bc4d20c6481456c2a89208
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2026-02-19 16:25:00 +0900

    [level 2] Title: 카펫, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -2,10 +2,9 @@ function solution(brown, yellow) {
     var answer = [];
     const total = brown + yellow;
     for (let height = 3; height <= Math.sqrt(total); height++){
-        if(total % height === 0){
+        if(total%height == 0){
             let width = total/height;
-            
-            if((width - 2) * (height - 2) === yellow){
+            if((width-2) * (height-2) == yellow){
                 return [width, height];
             }
         }
