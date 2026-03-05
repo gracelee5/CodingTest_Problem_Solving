7d92b1a8076f3ebf0b01fc5f4e07eb07547247ce
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2026-03-05 12:20:30 +0900

    [level 1] Title: 카드 뭉치, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,13 @@
+function solution(cards1, cards2, goal) {
+    var answer = '';
+    for(let i = 0; i<goal.length; i++){
+        if(goal[i] === cards1[0]){
+            cards1.splice(0,1);
+        }else if(goal[i] === cards2[0]){
+            cards2.splice(0,1);
+        }else{
+            return "No";
+        }
+    }
+    return "Yes";
+}
