c784c86258352de79b5efd803cc0cfa98b14a654
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2026-03-18 00:36:22 +0900

    [level 1] Title: 둘만의 암호, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,13 +1,12 @@
 function solution(s, skip, index) {
     var answer = '';
     var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
- 'n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    var newAlphabet = alphabet.filter((item) => !skip.includes(item));
+ 'n','o','p','q','r','s','t','u','v','w','x','y','z'].filter((item) => !skip.includes(item));
     var map = {}
-    newAlphabet.forEach((v,i)=>map[v] = i);
+    alphabet.forEach((v,i)=>map[v] = i);
     for(let i = 0; i<s.length; i++){
         let n = map[s[i]];//위치
-        answer += newAlphabet[(n+index)%(26-skip.length)];
+        answer += alphabet[(n+index)%(26-skip.length)];
     }
     return answer;
 }
