45616f048ff5333a0cc2a3bdb2eb1d4d271c48e5
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2026-03-24 13:19:18 +0900

    [level 2] Title: 소수 찾기, Time: 28.02 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 38 MB, 시간: 5.99 ms
+메모리: 37.7 MB, 시간: 28.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 18일 16:12:51
+2026년 03월 24일 13:19:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -3,41 +3,41 @@ function solution(numbers) {
     var numberEach = numbers.split("");
     var numberSet = new Set();
     
-    for (let i = 1; i<= numberEach.length; i++){
+    for(let i = 1; i<=numbers.length; i++){
         var numberList = [];
         var visited = new Array(numberEach.length).fill(false);
         recur(0,i,numberList);
     }
     
     for(const N of numberSet){
-        if(isPrime(N)) answer++;
+        if(isPrime(N))answer++;  
     }
     return answer;
     
-    function recur(idx, n, numberList){
-        if(idx == n){
-            var num = "";
+    function recur(idx,n,numberList){
+        if(idx === n){
+            var num = '';
             for(var i = 0; i<n; i++){
                 num += numberList[i];
             }
             numberSet.add(parseInt(num));
-            return;
+            return
         }
-        
-        for (var i = 0; i < numberEach.length; i++){
+        for(var i = 0; i<numberEach.length; i++){
             if(!visited[i]){
                 numberList[idx] = numberEach[i];
                 visited[i] = true;
-                recur(idx + 1, n, numberList);
+                recur(idx+1,n,numberList);
                 visited[i] = false;
             }
         }
     }
+    
     function isPrime(number){
-    if(number<2) return false;
-    for (let i = 2; i*i <=number; i++){
-        if(number % i == 0) return false;
-    }
-    return true;
+        if(number<2) return false;
+        for (let i = 2; i*i <=number; i++){
+            if(number % i == 0) return false;
+        }
+        return true;
     }
 }
