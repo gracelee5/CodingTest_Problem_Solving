66d625a53167a9050152989349b6c4c12fc5d748
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2026-04-03 10:35:07 +0900

    [level 2] Title: 피로도, Time: 30.15 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -2,9 +2,9 @@ function solution(k, dungeons) {
     var answer = 0;
     let visited = new Array(dungeons.length).fill(false);
     function dfs(currentK, count){
-        answer = Math.max(count, answer);
-        for(let i = 0; i < dungeons.length; i++){
-            if(!visited[i] && currentK >= dungeons[i][0]){
+        answer = Math.max(answer, count);
+        for (let i = 0; i < dungeons.length; i++){
+            if(currentK >= dungeons[i][0] && !visited[i]){
                 visited[i] = true;
                 dfs(currentK - dungeons[i][1], count + 1);
                 visited[i] = false;
