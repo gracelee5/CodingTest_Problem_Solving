8c126234feea1d4ecd7ae5635e3e9288080913de
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2026-04-04 12:44:33 +0900

    [level 3] Title: 네트워크, Time: 0.43 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,7 +1,6 @@
 function solution(n, computers) {
     var answer = 0;
-    const visited = [];
-    
+    var visited = [];
     for (let i = 0; i < n; i++){
         if(!visited[i]){
             dfs(i, visited, computers);
@@ -10,12 +9,12 @@ function solution(n, computers) {
     }
     return answer;
 }
-
-function dfs(node, visited, computers){
+function dfs (node, visited, computers){
     visited[node] = true;
-    for (let i = 0; i <computers.length; i++){
+    for (let i = 0; i<computers.length; i++){
         if(computers[node][i] === 1 && !visited[i]){
-            dfs(i,visited,computers);
+            dfs(i,visited, computers);
         }
     }
+    
 }
