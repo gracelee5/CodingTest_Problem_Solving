88a84eac06f3008c12fd38d9d3362d97ac638bf9
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2026-04-04 14:02:54 +0900

    [level 3] Title: 단어 변환, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -0,0 +1,27 @@
+function solution(begin, target, words) {
+    var answer = 0;
+    var visited = new Array(words.length).fill(false);
+    var queue = [[begin,0]];
+    if(!words.includes(target)){
+        return 0;
+    }
+    while(queue.length){
+        let [current, step] = queue.shift();
+        if(current === target) return step;
+        for(let i = 0; i<words.length; i++){
+            let diff = 0;
+            for(let j = 0; j <begin.length; j++){
+                if(words[i][j] !== current[j]){
+                    diff++;
+                    if(diff > 1){
+                        break;
+                    }
+                }
+            }
+            if(diff === 1 && !visited[i]){
+                visited[i] = true;
+                queue.push([words[i], step + 1]);
+            }
+        }
+    }
+}
