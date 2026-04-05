a394462e2395bbc705109938bcf5540aca0aca0e
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2026-04-05 12:34:21 +0900

    [level 3] Title: 정수 삼각형, Time: 3.41 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,10 +1,11 @@
 function solution(triangle) {
-    var answer = 0;
-    let dp = [...triangle[triangle.length - 1]];//얕은 복사
-    for (let i = triangle.length - 2; i >= 0; i--){ //밑에서부터 올라감
-        for(let j = 0; j <= i; j++){//열 탐색
+    let dp = [...triangle[triangle.length - 1]];
+    
+    for(let i = triangle.length - 2; i >= 0; i--){
+        for(let j = 0; j <= i; j++){
             dp[j] = triangle[i][j] + Math.max(dp[j],dp[j+1]);
         }
     }
-    return dp[0]
+    
+    return  dp[0];
 }
