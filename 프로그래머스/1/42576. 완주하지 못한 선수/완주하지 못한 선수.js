e02fe6886d7a52059c8b69922e9a27cbc68908b0
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2026-04-14 13:19:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 106.71 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,15 +1,11 @@
 function solution(participant, completion) {
-    var answer = '';
     participant.sort();
-    console.log(participant);
     completion.sort();
-    console.log(completion);
     
-    for(let i = 0; i <participant.length; i++){
+    for (let i = 0; i<participant.length; i++){
         if(participant[i] !== completion[i]){
-            answer = participant[i];
-            break;
+            return participant[i];
         }
     }
-    return answer;
+    return ;
 }
