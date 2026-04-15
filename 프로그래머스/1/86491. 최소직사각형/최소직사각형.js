69b39861ce8a9bba2ee474d325f19bd5a57219dc
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2026-04-16 01:27:19 +0900

    [level 1] Title: 최소직사각형, Time: 33.62 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,14 +1,10 @@
 function solution(sizes) {
-    let a = 0;//여기에 변이 더 작은걸
+    let a = 0;
     let b = 0;
     for(let size of sizes){
-        if(size[0]>size[1]){
-            a = Math.max(a,size[1]);
-            b = Math.max(b,size[0]);
-        }else{
-            a = Math.max(a,size[0]);
-            b = Math.max(b,size[1]);
-        }
+        size.sort((a,b)=>a-b);        
+        a = Math.max(a,size[1]);
+        b = Math.max(b,size[0]);
     }
     return a*b;
 }
