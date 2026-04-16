6866022ca059792571a53531f318640192d52e2b
LeeKyungwon <113881477+gracelee5@users.noreply.github.com>
2026-04-16 14:26:43 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -3,34 +3,34 @@ function solution(video_len, pos, op_start, op_end, commands) {
     function toSec(time){
         let min = Number(time.slice(0,2));
         let sec = Number(time.slice(3,5));
-        return sec + 60*min;
+        return min*60 + sec;
     }
+    video_len = toSec(video_len);
     pos = toSec(pos);
     op_start = toSec(op_start);
     op_end = toSec(op_end);
-    video_len = toSec(video_len);
-    for (let command of commands){
-        if(pos >= op_start && pos <= op_end){
+    
+    for(let command of commands){
+        if(pos>=op_start && pos<=op_end){
             pos = op_end;
         }
-        if(command === 'next'){
-            pos+=10;
-            if(pos >video_len){
-                pos = video_len;
-            }
-        }else if(command === 'prev'){
+        if(command === "next"){
+            pos += 10;
+        }else if(command === "prev"){
             pos -= 10;
-            if(pos < 0){
-                pos = 0;
-            }
         }
-        if(pos >= op_start && pos <= op_end){
+        
+        if(pos>video_len){
+            pos = video_len;
+        }else if(pos < 0){
+            pos = 0;
+        }
+        if(pos>=op_start && pos<=op_end){
             pos = op_end;
         }
     }
-    
-    answer += String(Math.floor(pos/60)).padStart(2,'0');
-    answer += ':';
-    answer += String(pos%60).padStart(2,'0');
+    answer += String(Math.floor(pos/60)).padStart(2,"0");
+    answer += ":";
+    answer += String(pos%60).padStart(2,"0");
     return answer;
 }
